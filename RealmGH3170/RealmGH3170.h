51200963113ddf49a5e21f1390bebac0a330f66e
JP Simard <jp@jpsim.com>
2016-02-23 14:32:22 -0800

    Initial Commit

---
+++ RealmGH3170/RealmGH3170.h
@@ -0,0 +1,19 @@
+//
+//  RealmGH3170.h
+//  RealmGH3170
+//
+//  Created by JP Simard on 2/23/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RealmGH3170.
+FOUNDATION_EXPORT double RealmGH3170VersionNumber;
+
+//! Project version string for RealmGH3170.
+FOUNDATION_EXPORT const unsigned char RealmGH3170VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmGH3170/PublicHeader.h>
+
+
